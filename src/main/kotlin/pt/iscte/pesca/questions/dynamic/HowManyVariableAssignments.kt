dd6922dbb0f8abcfc07a5fdfbf0789ac7b49a743
André L. Santos <andre.santos@iscte.pt>
2024-11-27 15:23:44 +0000

    - minors in layout and question text

--- src/main/kotlin/pt/iscte/pesca/questions/dynamic/HowManyVariableAssignments.kt
+++ src/main/kotlin/pt/iscte/pesca/questions/dynamic/HowManyVariableAssignments.kt
@@ -1,24 +1,22 @@
 package pt.iscte.pesca.questions.dynamic
 
 import pt.iscte.pesca.Language
-import pt.iscte.pesca.extensions.getUsedProceduresWithinModule
+import pt.iscte.pesca.extensions.correctAndRandomDistractors
 import pt.iscte.pesca.extensions.getVariableAssignments
 import pt.iscte.pesca.questions.QuestionData
-import pt.iscte.pesca.questions.SimpleTextOption
 import pt.iscte.pesca.questions.TextWithCodeStatement
 import pt.iscte.pesca.questions.subtypes.StrudelQuestionRandomProcedure
-import pt.iscte.strudel.model.IBlock
+import pt.iscte.strudel.model.ILoop
 import pt.iscte.strudel.model.IProcedure
 import pt.iscte.strudel.model.IVariableAssignment
 import pt.iscte.strudel.model.IVariableDeclaration
-import pt.iscte.strudel.model.IVariableExpression
 import pt.iscte.strudel.vm.IValue
 import pt.iscte.strudel.vm.IVirtualMachine
 
 class HowManyVariableAssignments : StrudelQuestionRandomProcedure() {
 
     val countPerVariable = mutableMapOf<IVariableDeclaration<*>, Int>()
-
+    var iterations = 0
     // There is at least one variable that is assigned multiple times.
     override fun isApplicable(element: IProcedure): Boolean =
         element.getVariableAssignments().any { it.value.size > 1 }
@@ -28,6 +26,10 @@ class HowManyVariableAssignments : StrudelQuestionRandomProcedure() {
         vm.addListener(object : IVirtualMachine.IListener {
             override fun variableAssignment(a: IVariableAssignment, value: IValue) {
                 countPerVariable[a.target] = (countPerVariable[a.target] ?: 0) + 1
+            }
+
+            override fun loopIteration(loop: ILoop) {
+                iterations++
             }
         })
     }
@@ -42,19 +44,18 @@ class HowManyVariableAssignments : StrudelQuestionRandomProcedure() {
     ): QuestionData {
         vm.execute(procedure, *arguments.toTypedArray())
 
-        val declarations = procedure.getVariableAssignments().keys.size
         val variable = countPerVariable.keys.random()
         val count = countPerVariable[variable]!!
 
         return QuestionData(
             TextWithCodeStatement(language["HowManyVariableAssignments"].format(variable.id, call), procedure),
-            mapOf(
-                SimpleTextOption(count) to true,
-                SimpleTextOption(count+1) to false,
-                SimpleTextOption(if(declarations != count) declarations else count-1) to false,
-                (if (count != 0) SimpleTextOption(0) else SimpleTextOption.none(language)) to false,
-            ),
-            language = language
+            correctAndRandomDistractors(count,
+                setOf(
+                    count+1,
+                    count-1,
+                    iterations,
+            ) + countPerVariable.map { it.value }),
+            language
         )
     }
 }
