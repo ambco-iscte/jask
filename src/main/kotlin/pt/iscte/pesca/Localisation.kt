f7d482d95c123bd04781338131cc0a6969a152cb
André L. Santos <andre.santos@iscte.pt>
2025-02-05 18:29:59 +0000

    um pouco ajavardado, mas é o possível
    nova QLC: WhichParameterTypes

--- src/main/kotlin/pt/iscte/pesca/Localisation.kt
+++ src/main/kotlin/pt/iscte/pesca/Localisation.kt
@@ -2,15 +2,28 @@ package pt.iscte.pesca
 
 import java.io.File
 import java.io.FileInputStream
+import java.io.StringReader
+import java.net.URLDecoder
 import java.util.Properties
+import java.util.jar.JarFile
 
 object Localisation {
     internal val languages = mutableSetOf<Language>()
 
     init {
-        val root = File(Localisation::class.java.getResource("/localisation")!!.path)
-        root.listFiles()?.forEach { register(it) }
+        languages.add(Language("en", Properties().apply {
+            load(StringReader(loadResource("localisation/en.properties")!!))
+        }))
+        languages.add(Language("pt", Properties().apply {
+            load(StringReader(loadResource("localisation/pt.properties")!!))
+        }))
     }
+
+    fun loadResource(path: String): String? {
+        return javaClass.classLoader.getResourceAsStream(path)?.bufferedReader(Charsets.ISO_8859_1)?.use { it.readText() }
+    }
+
+
 
     val DefaultLanguage: Language = languages.first { it.code ==  "en" }
 
@@ -18,17 +31,19 @@ object Localisation {
         val code = file.nameWithoutExtension
         if (languages.any { it.code == code })
             throw IllegalArgumentException("Cannot register language ${file.name}: duplicated code $code!")
-        val lang = Language(code, file)
+        val lang = Language(code, Properties().apply { load(file.inputStream()) })
         languages.add(lang)
         return lang
     }
+
+
 
     fun getLanguage(code: String): Language =
         languages.firstOrNull { it.code == code } ?: DefaultLanguage
 }
 
-data class Language(val code: String, val file: File) {
-    private val properties = Properties().apply { load(file.inputStream()) }
+data class Language(val code: String, val properties: Properties) {
+    //private val properties = Properties().apply { load(file.inputStream()) }
 
     companion object {
         val DEFAULT: Language = Localisation.DefaultLanguage
@@ -43,9 +58,9 @@ data class Language(val code: String, val file: File) {
 }
 
 fun main() {
-    Localisation.languages.forEach {
-        println("${it.code}: ${it.file.path}")
-    }
-    val pt = Localisation.getLanguage("pt")
-    println(pt.getLocalisation("HowManyParameters"))
+    //Localisation.languages.forEach {
+       // println("${it.code}: ${it.file.path}")
+    //}
+    val pt = Localisation.getLanguage("en")
+    println(pt.getLocalisation("HowManyParams"))
 }
---
+++ src/main/kotlin/pt/iscte/pesca/questions/fixed/WhichParameterTypes.kt
@@ -0,0 +1,59 @@
+package pt.iscte.pesca.questions.fixed
+
+import com.github.javaparser.ast.body.MethodDeclaration
+import pt.iscte.pesca.Language
+import pt.iscte.pesca.extensions.getLocalVariables
+import pt.iscte.pesca.questions.Option
+import pt.iscte.pesca.questions.QuestionData
+import pt.iscte.pesca.questions.SimpleTextOption
+import pt.iscte.pesca.questions.TextWithCodeStatement
+import pt.iscte.pesca.questions.subtypes.JavaParserQuestionRandomMethod
+import kotlin.collections.plus
+
+class WhichParameterTypes : JavaParserQuestionRandomMethod() {
+
+    override fun isApplicable(element: MethodDeclaration): Boolean =
+        element.parameters.isNotEmpty() || element.getLocalVariables().isNotEmpty()
+
+    override fun build(method: MethodDeclaration, language: Language): QuestionData {
+        val parameters = method.parameters.map { it.nameAsString }
+        val paramTypes = method.parameters.map { it.typeAsString }
+        val returnType = method.typeAsString
+
+        val localVars = method.getLocalVariables().map { it.nameAsString }
+        val localVarTypes = method.getLocalVariables().map { it.typeAsString }
+
+        val methodName = method.nameAsString
+
+        val options: Map<Option, Boolean> =
+            if (parameters.isNotEmpty() && localVars.isNotEmpty()) // Method has both parameters and local variables.
+                mapOf(
+                    SimpleTextOption(parameters) to false,
+                    SimpleTextOption(paramTypes) to true,
+                    SimpleTextOption(returnType) to false,
+                    SimpleTextOption(parameters + localVars) to false
+                )
+            else if (parameters.isNotEmpty()) // Method only has parameters.
+                mapOf(
+                    SimpleTextOption(parameters) to false,
+                    SimpleTextOption(parameters + listOf(methodName)) to false,
+                    SimpleTextOption(paramTypes) to true,
+                    SimpleTextOption(returnType + listOf(methodName)) to false,
+                )
+            else if (localVars.isNotEmpty()) // Method only has local variables.
+                mapOf(
+                    SimpleTextOption(localVars) to false,
+                    SimpleTextOption(localVars + listOf(methodName)) to false,
+                    SimpleTextOption(localVarTypes) to false,
+                    SimpleTextOption(language["FunctionTakesNoParameters"]) to true,
+                )
+            else
+                emptyMap() // This case is never applied, as per the isApplicable method.
+
+        return QuestionData(
+            TextWithCodeStatement(language[this::class.simpleName!!].format(method.nameAsString), method),
+            options,
+            language = language
+        )
+    }
+}
