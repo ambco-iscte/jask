4255318e96ece4e9d62f98585dcd4fb052a29ae9
serrano777 <goncalo.m.c.serrano@gmail.com>
2025-05-23 14:36:42 +0100

    add distractors to UselessDuplicationIfElse

--- src/main/kotlin/pt/iscte/pesca/templates/codeSmells/UselessDuplicationIfElse.kt
+++ src/main/kotlin/pt/iscte/pesca/templates/codeSmells/UselessDuplicationIfElse.kt
@@ -1,6 +1,7 @@
 package pt.iscte.pesca.templates
 
 import com.github.javaparser.ast.body.MethodDeclaration
+import com.github.javaparser.ast.expr.BooleanLiteralExpr
 import com.github.javaparser.ast.nodeTypes.NodeWithBody
 import com.github.javaparser.ast.stmt.BlockStmt
 import com.github.javaparser.ast.stmt.IfStmt
@@ -18,11 +19,33 @@ class UselessDuplicationIfElse : StaticQuestionTemplate<MethodDeclaration>() {
         val (source, method) = sources.getRandom<MethodDeclaration>()
 
         val methodReplaced = method.clone()
+        val methodReplacedWA = method.clone()
+        val methodReplacedWA2 = method.clone()
+        val methodReplacedRA = method.clone()
 
-        // Find a random IfStmt that has duplicate code
+
         val ifStmt = methodReplaced.findAll(IfStmt::class.java)
             .filter { it.hasDuplicateCode() }
-            .random()
+            .first()
+
+        val ifStmtWA = methodReplacedWA.findAll(IfStmt::class.java)
+            .filter { it.hasDuplicateCode() }
+            .first()
+
+        val ifStmtWA2 = methodReplacedWA2.findAll(IfStmt::class.java)
+            .filter { it.hasDuplicateCode() }
+            .first()
+
+        val ifStmtRA = methodReplacedRA.findAll(IfStmt::class.java)
+            .filter { it.hasDuplicateCode() }
+            .first()
+
+        ifStmtWA.removeElseStmt()
+        ifStmtWA2.removeElseStmt()
+        ifStmtWA2.setCondition(negateExpression(ifStmtWA2.condition))
+        ifStmtRA.removeElseStmt()
+        ifStmtRA.setCondition(BooleanLiteralExpr(true))
+
 
         val thenStmt = ifStmt.thenStmt
         val parent = ifStmt.parentNode.get()
@@ -52,9 +75,14 @@ class UselessDuplicationIfElse : StaticQuestionTemplate<MethodDeclaration>() {
             source,
             TextWithMultipleCodeStatements(
                 language["UselessDuplicationIfElse"].format(method.nameAsString),
-                listOf(method.toString(), methodReplaced.toString())
+                listOf(method.toString())
             ),
-            true.trueOrFalse(language),
+            mapOf(
+                SimpleTextOption(methodReplaced.toString()) to true,
+                SimpleTextOption(methodReplacedWA.toString()) to false,
+                SimpleTextOption(methodReplacedWA2.toString()) to false,
+                SimpleTextOption(methodReplacedRA.toString()) to true
+            ),
             language = language
 
         )
